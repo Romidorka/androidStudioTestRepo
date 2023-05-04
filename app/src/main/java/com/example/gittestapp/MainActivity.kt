815a231c71928dad31e3b7dae97cab34c1267b10
Magistr Of Code <itsnotreal@email.com>
2023-05-04 20:21:36 +0600

    Program done.

--- app/src/main/java/com/example/gittestapp/MainActivity.kt
+++ app/src/main/java/com/example/gittestapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gittestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.btn)
+        btn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Ti loh", Toast.LENGTH_LONG).show()
+        }
     }
 }
